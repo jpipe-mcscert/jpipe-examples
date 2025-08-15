2330f04eca8d5786ca5f71ee6e2d28d82fd92818
Nicolas Lacroix <nicolas.lacroix@univ-cotedazur.fr>
2025-08-15 14:22:12 -0400

    feat(steps): Add POC for multilingual_dataset_is_present

--- llm_FATES/jpipe-libs/steps.py
+++ llm_FATES/jpipe-libs/steps.py
@@ -1,13 +1,19 @@
 ######
 ## Justification final
 ######
+import yaml
+
+from typing import Any, Callable
 
 from jpipe_runner.framework.decorators.jpipe_decorator import jpipe
 
 
+RESOURCES_DIR = "resources/"
+
 ################
 ## Conclusion ##
 ################
+
 
 ## Conclusion c
 @jpipe(consume=[])
@@ -18,6 +24,7 @@ def fairness_exists() -> bool:
 ###############
 ## Strategy  ##
 ###############
+
 
 ## Strategy training
 @jpipe(consume=[], produce=[])
@@ -59,6 +66,7 @@ def evaluating_model_using_the_bbq_benchmark(produce) -> bool:
 ## Evidence  ##
 ###############
 
+
 ## Evidence multi_ling_BM
 @jpipe(consume=[], produce=[])
 def multilingual_benchmark_is_present(produce) -> bool:
@@ -78,9 +86,49 @@ def bbq_benchmark_is_present(produce) -> bool:
 
 
 ## Evidence multi_ling_DS
-@jpipe(consume=[], produce=[])
-def multilingual_dataset_is_present(produce) -> bool:
-    return True
+@jpipe(consume=[], produce=["dataset_configuration"])
+def multilingual_dataset_is_present(produce: Callable[[str, Any], None]) -> bool:
+    EXPECTED_SMOLLM3_MODEL_NAME = "smollm3-3B-final"
+    EXPECTED_PRETRAINING_LANGUAGES = [
+        "fw2-fra",
+        "fw2-spa",
+        "fw2-deu",
+        "fw2-ita",
+        "fw2-por",
+        "fw2-cmn",
+        "fw2-rus",
+        "fw2-fas",
+        "fw2-jpn",
+        "fw2-kor",
+        "fw2-hin",
+        "fw2-tha",
+        "fw2-vie",
+        "fw2-ell",
+    ]
+
+    with open(f"{RESOURCES_DIR}/pretraining/stage1_8T.yaml") as f:
+        try:
+            pretraining_stage1_run_configuration = yaml.safe_load(f)
+            # comparing the model name to be sure it is smollm3
+            assert (
+                pretraining_stage1_run_configuration["general"]["project"]
+                == EXPECTED_SMOLLM3_MODEL_NAME
+            )
+            # comparing the listed datasets with the expected supported languages
+            dataset_configuration = pretraining_stage1_run_configuration["data_stages"][
+                0
+            ]["data"]["dataset"]
+            all_pretraining_datasets = dataset_configuration["dataset_read_path"]
+            for pretraining_language in EXPECTED_PRETRAINING_LANGUAGES:
+                assert (
+                    f"/scratch/smollm3-data-part1/{pretraining_language}"
+                    in all_pretraining_datasets
+                ), f"missing language [{pretraining_language}] in pretraining datasets"
+            produce("dataset_configuration", dataset_configuration)
+            return True
+        except (yaml.YAMLError, AssertionError) as exc:
+            print(exc)
+            return False
 
 
 ## Evidence evaluated_code
