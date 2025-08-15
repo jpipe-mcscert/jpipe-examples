56e43e117de8be11b7c8497d55b139b34ac380a1
Nicolas Lacroix <nicolas.lacroix@univ-cotedazur.fr>
2025-08-15 13:50:41 -0400

    feat(operations): Add generated base operations for justification.jd

---
+++ llm_FATES/jpipe-libs/steps.py
@@ -0,0 +1,53 @@
+######
+## Justification final
+######
+
+## Conclusion c
+def fairness_exists() -> bool:
+    return True
+
+## Evidence multi_ling_BM
+def multilingual_benchmark_is_present() -> bool:
+    return True
+
+## Strategy training
+def training_model_using_the_multilingual_dataset() -> bool:
+    return True
+
+## Evidence training_code
+def training_code_is_present() -> bool:
+    return True
+
+## Strategy testing_multi_ling
+def evaluating_model_using_the_multilingual_benchmark() -> bool:
+    return True
+
+## Strategy fairness_impl_methods
+def implementing_fairness_within_model() -> bool:
+    return True
+
+## Evidence BBQ_BM
+def bbq_benchmark_is_present() -> bool:
+    return True
+
+## Strategy AND
+def and_() -> bool:
+    return True
+
+## Evidence multi_ling_DS
+def multilingual_dataset_is_present() -> bool:
+    return True
+
+## Strategy fairness_exe_methods
+def executing_fairness_benchmarks() -> bool:
+    return True
+
+## Strategy testing_BBQ
+def evaluating_model_using_the_bbq_benchmark() -> bool:
+    return True
+
+## Evidence evaluated_code
+def evaluation_code_is_present() -> bool:
+    return True
+
+
