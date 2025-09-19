bfb8dc5290fd73920d668c040fa1e36b91a9ecfa
Kalvin Khuu <77596289+kalvinkhuu@users.noreply.github.com>
2025-09-19 17:07:18 -0400

    test

---
+++ llm_FATES/compiler/cyber_capability.py
@@ -0,0 +1,33 @@
+######
+## Justification cyber_capability
+######
+
+## Strategy verify_ctf_cap_acceptable
+def verify_ctf_challenges_capability_is_acceptable() -> bool:
+    return False
+
+## Evidence headlesslinux_distro
+def headlesslinux_distribution_used_for_ctf() -> bool:
+    return False
+
+## Strategy verify_adversfine_cap_cybersec_acceptable
+def aggregate_all_the_models_evaluations_in_terms_of_cybersecurity_capabilities_() -> bool:
+    return False
+
+## Evidence curated_ctf_challenges_list
+def curated_ctf_challenges_list() -> bool:
+    return True
+
+## Evidence ctf_eval_results
+def ctf_challenges_evaluation_results() -> bool:
+    return True
+
+## Conclusion adversfine_cap_cybersec_acceptable
+def adversarially_finetuned_models_capability_in_cybersecurity_is_acceptable() -> bool:
+    return False
+
+## Evidence threshold_for_model_ctf_performance
+def threshold_for_ctf_model_performance() -> bool:
+    return True
+
+
