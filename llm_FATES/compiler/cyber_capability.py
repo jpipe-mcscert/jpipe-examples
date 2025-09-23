35a426662870e9441b68d5e888054e60824ada38
Kalvin Khuu <77596289+kalvinkhuu@users.noreply.github.com>
2025-09-23 13:49:02 -0400

    added variables on the consume and the produce

--- llm_FATES/compiler/cyber_capability.py
+++ llm_FATES/compiler/cyber_capability.py
@@ -3,38 +3,38 @@
 ######
 
 ## Strategy verify_ctf_cap_acceptable
-@jpipe(consume=[], produce=[])
-def verify_ctf_challenges_capability_is_acceptable() -> bool:
+@jpipe(consume=["test1", "test2", "test3", "test4"], produce=["test5"])
+def verify_ctf_challenges_capability_is_acceptable(produce: Callable[[str, Any], None]) -> bool:
     return False
 
 ## Evidence headlesslinux_distro
-@jpipe(consume=[], produce=[])
-def headlesslinux_distribution_used_for_ctf() -> bool:
+@jpipe(consume=[], produce=["test1"])
+def headlesslinux_distribution_used_for_ctf(produce: Callable[[str, Any], None]) -> bool:
     return False
 
 ## Strategy verify_adversfine_cap_cybersec_acceptable
-@jpipe(consume=[], produce=[])
-def aggregate_all_the_models_evaluations_in_terms_of_cybersecurity_capabilities_() -> bool:
+@jpipe(consume=["test5"], produce=[])
+def aggregate_all_the_models_evaluations_in_terms_of_cybersecurity_capabilities_(produce: Callable[[str, Any], None]) -> bool:
     return False
 
 ## Evidence curated_ctf_challenges_list
-@jpipe(consume=[], produce=[])
-def curated_ctf_challenges_list() -> bool:
+@jpipe(consume=[], produce=["test2"])
+def curated_ctf_challenges_list(produce: Callable[[str, Any], None]) -> bool:
     return True
 
 ## Evidence ctf_eval_results
-@jpipe(consume=[], produce=[])
-def ctf_challenges_evaluation_results() -> bool:
+@jpipe(consume=[], produce=["test3"])
+def ctf_challenges_evaluation_results(produce: Callable[[str, Any], None]) -> bool:
     return True
 
 ## Conclusion adversfine_cap_cybersec_acceptable
 @jpipe(consume=[])
-def adversarially_finetuned_models_capability_in_cybersecurity_is_acceptable() -> bool:
+def adversarially_finetuned_models_capability_in_cybersecurity_is_acceptable(produce: Callable[[str, Any], None]) -> bool:
     return False
 
 ## Evidence threshold_for_model_ctf_performance
-@jpipe(consume=[], produce=[])
-def threshold_for_ctf_model_performance() -> bool:
+@jpipe(consume=[], produce=["test4"])
+def threshold_for_ctf_model_performance(produce: Callable[[str, Any], None]) -> bool:
     return True
 
 
