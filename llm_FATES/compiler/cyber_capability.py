b7819943c15e01f77f31ab063ba0b9305b073186
Kalvin Khuu <77596289+kalvinkhuu@users.noreply.github.com>
2025-09-23 13:30:29 -0400

    added the consume and produce

--- llm_FATES/compiler/cyber_capability.py
+++ llm_FATES/compiler/cyber_capability.py
@@ -3,30 +3,37 @@
 ######
 
 ## Strategy verify_ctf_cap_acceptable
+@jpipe(consume=[], produce=[])
 def verify_ctf_challenges_capability_is_acceptable() -> bool:
     return False
 
 ## Evidence headlesslinux_distro
+@jpipe(consume=[], produce=[])
 def headlesslinux_distribution_used_for_ctf() -> bool:
     return False
 
 ## Strategy verify_adversfine_cap_cybersec_acceptable
+@jpipe(consume=[], produce=[])
 def aggregate_all_the_models_evaluations_in_terms_of_cybersecurity_capabilities_() -> bool:
     return False
 
 ## Evidence curated_ctf_challenges_list
+@jpipe(consume=[], produce=[])
 def curated_ctf_challenges_list() -> bool:
     return True
 
 ## Evidence ctf_eval_results
+@jpipe(consume=[], produce=[])
 def ctf_challenges_evaluation_results() -> bool:
     return True
 
 ## Conclusion adversfine_cap_cybersec_acceptable
+@jpipe(consume=[])
 def adversarially_finetuned_models_capability_in_cybersecurity_is_acceptable() -> bool:
     return False
 
 ## Evidence threshold_for_model_ctf_performance
+@jpipe(consume=[], produce=[])
 def threshold_for_ctf_model_performance() -> bool:
     return True
 
