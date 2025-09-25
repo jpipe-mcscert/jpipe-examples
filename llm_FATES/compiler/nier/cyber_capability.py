32a74ea3ce3e0ae3c739463983f449d7c81d8ed3
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2025-09-25 22:12:53 +0200

    comment cyber_range_capability_is_acceptable

--- llm_FATES/compiler/nier/cyber_capability.py
+++ llm_FATES/compiler/nier/cyber_capability.py
@@ -7,10 +7,10 @@ from typing import Any, Callable
 from jpipe_runner.framework.decorators.jpipe_decorator import jpipe
 
 ## Evidence cyber_range
-@jpipe(produce=["test7"])
-def cyber_range_capability_is_acceptable(produce: Callable[[str, Any], None]) -> bool:
-    produce("test7", "test7")
-    return True
+# @jpipe(produce=["test7"])
+# def cyber_range_capability_is_acceptable(produce: Callable[[str, Any], None]) -> bool:
+#     produce("test7", "test7")
+#     return True
 
 ## Strategy verify_ctf_cap_acceptable
 @jpipe(consume=["test1", "test2", "test3", "test4", "test6"], produce=["test5"])
@@ -33,10 +33,10 @@ def headless_linux_distribution_used_for_ctf(produce: Callable[[str, Any], None]
     return True
 
 ## Strategy verify_adversfine_cap_cybersec_acceptable
-@jpipe(consume=["test5", "test7"])
-def aggregate_all_the_models_evaluations_in_terms_of_cybersecurity_capabilities(test5: str, test7: str) -> bool:
+@jpipe(consume=["test5"])
+def aggregate_all_the_models_evaluations_in_terms_of_cybersecurity_capabilities(test5: str) -> bool:
     print(test5)
-    print(test7)
+    # print(test7)
     return True
 
 ## Evidence curated_ctf_challenges_list
@@ -52,10 +52,10 @@ def ctf_challenges_evaluation_results(produce: Callable[[str, Any], None]) -> bo
     return True
 
 ## Conclusion adversfine_cap_cybersec_acceptable
-@jpipe(consume=["test5", "test7"])
-def adversarially_finetuned_models_capability_in_cybersecurity_is_acceptable(test5: str, test7: str) -> bool:
+@jpipe(consume=["test5"])
+def adversarially_finetuned_models_capability_in_cybersecurity_is_acceptable(test5: str) -> bool:
     print(test5)
-    print(test7)
+    # print(test7)
     return True
 
 ## Evidence threshold_for_model_ctf_performance
